0bebe65123281f9a0a86103f512f738d89612717
Dhillips19 <daniel.phillips@outlook.ie>
2025-04-13 18:00:14 +0100

    new changes

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/tests/setupTests.js'],  // Updated path
+  setupFilesAfterEnv: ['<rootDir>/__tests__/setupTests.js'],  // Updated path
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': '<rootDir>/tests/__mocks__/styleMock.js',  // Updated path
-    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/tests/__mocks__/fileMock.js'  // Updated path
+    '\\.(css|less|scss|sass)$': '<rootDir>/__tests__/__mocks__/styleMock.js',  // Updated path
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__tests__/__mocks__/fileMock.js'  // Updated path
   },
   transform: {
     '^.+\\.(js|jsx)$': 'babel-jest'
   },
   testMatch: [
-    "<rootDir>/tests/**/*.test.{js,jsx}"
+    "<rootDir>/__tests__/**/*.test.{js,jsx}"
   ],
   moduleDirectories: ["node_modules", "src"]
 };
